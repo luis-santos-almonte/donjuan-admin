ba1593741859c24f597de5b6bea56bf89e1676e5
luis-santos-almonte <luisreynaldosa@gmail.com>
2024-03-17 02:00:41 -0300

    CREACION DE TODO

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MasterLayout from "./layouts/admin/MasterLayout";
+
+function App() {
+  return <div className="App">
+    <Router>
+      <Routes>
+        <Route path="/admin/dashboard" element={<MasterLayout/>}/>
+      </Routes>
+    </Router>
+  </div>;
+}
+
+export default App;
